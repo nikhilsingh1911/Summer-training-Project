2d57a38265893de39ece39f345d669bd908f4acc
nikhilsingh1911 <115785542+nikhilsingh1911@users.noreply.github.com>
2024-07-06 17:54:58 +0530

    fornt-client-done-till-navbar

--- Book Rental App/fornt-client/src/App.jsx
+++ Book Rental App/fornt-client/src/App.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      {/* <h1 className="text-8xl font-bold underline">Hello Nikhil.</h1> */}
-      <nav>Site logo</nav>
+      <Navbar />
       <Outlet />
     </>
   );
---
+++ Book Rental App/fornt-client/src/components/Navbar.jsx
@@ -0,0 +1,113 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { GiBurningBook } from "react-icons/gi";
+import { FaBarsStaggered } from "react-icons/fa6";
+import { FaXmark } from "react-icons/fa6";
+
+const Navbar = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const [isStickDay, setSticy] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setSticy(true);
+      } else {
+        setSticy(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.addEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const navItems = [
+    { link: "Home", path: "/" },
+    { link: "About", path: "/about" },
+    { link: "Shop", path: "/shop" },
+    { link: "Sell your Book", path: "/admin/dashboard" },
+    { link: "Blog", path: "/blog" },
+  ];
+  return (
+    <>
+      {/* <div>Navbar</div> */}
+      <header className="w-full bg-transparent fixed top-0 left-0 right-0 transition-all ease-in duration-300">
+        {/* something left */}
+        <nav
+          className={` py-4 lg:px-24 px-4 ${
+            isStickDay ? "sticky top-0 left-0 right-0 bg-blue-200" : ""
+          }`}
+        >
+          <div className="flex justify-between items-center text-base gap-8">
+            {/*Logo */}
+            <Link
+              to="/"
+              className="text-2xl font-bold text-blue-800 flex item-center gap-2"
+            >
+              <GiBurningBook className="inline-block" />
+              Books
+            </Link>
+
+            <ul className="md:flex space-x-12 hidden">
+              {navItems.map(({ link, path }) => (
+                <Link
+                  key={path}
+                  to={path}
+                  className="block text-base text-black uppercase cursor-pointer hover:text-blue-700"
+                >
+                  {link}
+                </Link>
+              ))}
+            </ul>
+            <div className="space-x-12 hidden lg:flex item center">
+              <button>
+                {/* navigation bar three lines */}
+                <FaBarsStaggered className="w-5 hover:text-blue-700" />
+              </button>
+            </div>
+
+            {/* menu btn for the mobile devices */}
+
+            <div className="md:hidden">
+              <button
+                onClick={toggleMenu}
+                className="text-black focus: outline-none"
+              >
+                {isMenuOpen ? (
+                  <FaXmark className="h-5 w-5 text-black" />
+                ) : (
+                  <FaBarsStaggered />
+                )}
+              </button>
+            </div>
+          </div>
+
+          {/* nav item for small devices */}
+          <div
+            className={`space-y-4 px-4 mt-16 py-7 bg-blue-600 ${
+              isMenuOpen ? "block fixed top-0 right-0 left-0" : "hidden"
+            }`}
+          >
+            {navItems.map(({ link, path }) => (
+              <Link
+                key={path}
+                to={path}
+                className="block text-base text-white uppercase cursor-pointer"
+              >
+                {link}
+              </Link>
+            ))}
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
